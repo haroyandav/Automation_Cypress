a387fb096167884f07dc8522a7719eff19888e8b
haroyandav <haroyandav@gmail.com>
2023-04-17 13:12:17 +0400

    Changes for the file commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -24,3 +24,14 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
 
+
+
+
+Cypress.Commands.add("login", (email, password) => { 
+
+        cy.visit('https://admin-demo.nopcommerce.com/login')
+        cy.get('input[name=Email]').clear().type(email)
+        cy.get('input[name=Password]').clear().type(password)
+        cy.get('.button-1').click()
+
+})
