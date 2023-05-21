17c36cb6c34baf4e065620cd285390b966b78e26
haroyandav <haroyandav@gmail.com>
2023-05-21 19:33:16 +0400

    21.05.2023 Js start

--- JavaScript and Node.js Essentials for Test Automation/Variable.js
+++ JavaScript and Node.js Essentials for Test Automation/Variable.js
@@ -21,6 +21,15 @@ if(true) {
 
     var count2 = 10
 
-    console.log("The value of count inside block ", count);
+    console.log("The value of count inside block 2 ", count2);
 }
-console.log("The value of count outside block ",count);
+console.log("The value of count outside block 2 ",count2);
+
+const count3 = 0 
+if(true) {
+
+    const count3 = 10
+
+    console.log("The value of count inside block 3 ", count3);
+}
+console.log("The value of count outside block 3 ",count3);
