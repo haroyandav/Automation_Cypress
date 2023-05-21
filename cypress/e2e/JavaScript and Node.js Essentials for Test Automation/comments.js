93ed07bdd45024d12220d8fc34b5ec1e21d8aba2
haroyandav <haroyandav@gmail.com>
2023-05-21 19:14:16 +0400

    JS

---
+++ cypress/e2e/JavaScript and Node.js Essentials for Test Automation/comments.js
@@ -0,0 +1,13 @@
+// My first program 
+console.log('Hello world')
+
+/**
+ * Add two numbers
+ * @param {*} sum1 
+ * @param {*} sum2 
+ * @returns sum of two numbers
+ */
+function sum(sum1, sum2){
+
+    return sum1 + sum2 // return type is a number
+}
---
+++ cypress/e2e/JavaScript and Node.js Essentials for Test Automation/variable.js
@@ -0,0 +1,26 @@
+// My first program 
+console.log('Hello world')
+
+let greeting = 'Hello world by using variable' , greeting1 = 'Bye bye'
+// console.log(greeting);
+// console.log(greeting1);
+console.log(greeting + ',' ,greeting1); 
+
+
+let count = 0 
+if(true) {
+
+    let count = 10
+    console.log("The value of count inside block ", count);
+}
+console.log("The value of count outside block ",count);
+
+
+var count2 = 0 
+if(true) {
+
+    var count2 = 10
+
+    console.log("The value of count inside block ", count);
+}
+console.log("The value of count outside block ",count);
