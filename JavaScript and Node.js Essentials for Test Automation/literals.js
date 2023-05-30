11f60a87f73b17f6f0e6b6dbefd05ee36c7e172c
haroyandav <haroyandav@gmail.com>
2023-05-30 15:59:40 +0400

    Data types

---
+++ JavaScript and Node.js Essentials for Test Automation/literals.js
@@ -0,0 +1,14 @@
+let str = 'Hello'
+let str1 = "Hello"
+let str3 = `${str},World`
+console.log(str3);
+
+let num = 5
+let num1 = 5.5
+
+let bool = true
+let bool1 = false
+
+console.log(`The type of string is: ${typeof str3}`);
+console.log(`The type of boolean is: ${typeof bool1}`);
+console.log(`The type of number is: ${typeof num}`);
