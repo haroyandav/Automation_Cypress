b35d2a18c1e225d03dbbd3c01ae6b03124828853
haroyandav <haroyandav@gmail.com>
2023-06-20 17:41:47 +0400

    Zero

---
+++ cypress/e2e/Cypress_Web Automation ZeroToHero/loops.js
@@ -0,0 +1,21 @@
+
+// Loop
+
+for (let i = 1; i <= 5; i++){
+    console.log('Hello World');
+}
+
+let cars = ['Volvo' , 'Audi' , 'Lexus' , 'BMW' , 'Benz']
+
+for (let car of cars) {
+    console.log(car);
+    if (car === 'BMW') {
+        break
+    }
+}
+
+// ES6 syntax 
+
+cars.forEach((car) => {
+    console.log(car)
+})
